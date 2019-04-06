16d2728c84f689a354909e59177bd598dd14fc29
Ju Zhang <jzha263@aucklanduni.ac.nz>
2019-04-06 16:57:05 +1300

    Clip values in tolerance to 1 before arccos

--- math.py
+++ math.py
@@ -15,6 +15,7 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import numpy as np
 import scipy.stats as stats
 
+
 def norm( v ):
     """
     Normalise a vector v
@@ -22,20 +23,29 @@ def norm( v ):
     # return np.divide(v, np.sqrt((np.array(v)**2.0).sum()))
     return np.divide(v, mag(v))
 
+
 def norms(v):
     """
     Normalise a list of vectors v
     """
     return np.divide(v, mag(v)[:,np.newaxis])
 
+
 def mag( v ):
     return np.sqrt((np.array(v)**2.0).sum(-1))
 
-def angle(v1, v2):
-    return np.arccos((v1*v2).sum(-1)/(mag(v1)*mag(v2)))
+
+def angle(v1, v2, tol=1e-9):
+    x = (v1*v2).sum(-1)/(mag(v1)*mag(v2))
+    if abs(1.0-x)<=tol:
+        return 0.0
+    else:
+        return np.arccos(x)
+
 
 def rms(x):
     return np.sqrt((x*x).mean(-1))
+
 
 def meanConfidenceInterval(data, confidence=0.95):
     a = 1.0*np.array(data)
@@ -43,6 +53,7 @@ def meanConfidenceInterval(data, confidence=0.95):
     m, se = np.mean(a), stats.sem(a)
     h = se * stats.t.ppf((1+confidence)/2., n-1)
     return m, m-h, m+h
+
 
 def trimAngle(x):
     return np.mod(x, 2*np.pi*np.sign(x))
